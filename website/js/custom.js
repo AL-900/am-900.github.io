9cb6596cf9e85d684dd6c9f11a54d1123cf0b995
AL-900 <62214691+AL-900@users.noreply.github.com>
2020-03-15 23:54:36 +0600

    new

---
+++ website/js/custom.js
@@ -0,0 +1,120 @@
+(function($){
+	$(document).ready(function(){
+		
+		$('#s').pieChart({
+			
+			barColor: '#341f97',
+			trackColor: '#c8d6e5',
+			lineWidth: 12,
+			size: 170,
+			rotate: 0,
+			animate: {
+			duration: 4000,
+			enabled: true
+					},
+
+		});
+		// ==
+		
+		$('#c').pieChart({
+			
+			barColor: '#f53b57',
+			trackColor: '#c8d6e5',
+			lineWidth: 12,
+			size: 170,
+			rotate: 0,
+			animate: {
+			duration: 4000,
+			enabled: true
+					},
+
+		});
+		// ==
+		
+		$('#j').pieChart({
+			
+			barColor: '#ffa801',
+			trackColor: '#c8d6e5',
+			lineWidth: 12,
+			size: 170,
+			rotate: 0,
+			animate: {
+			duration: 4000,
+			enabled: true
+					},
+
+		});
+		// ==
+		
+		$('#b').pieChart({
+			
+			barColor: '#2e86de',
+			trackColor: '#c8d6e5',
+			lineWidth: 12,
+			size: 170,
+			rotate: 0,
+			animate: {
+			duration: 4000,
+			enabled: true
+					},
+		});
+		// ==
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
